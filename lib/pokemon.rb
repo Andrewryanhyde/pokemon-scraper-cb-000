d9323bd8af03a6989091e48aa8489f4963a12a9c
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-10 14:39:27 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,8 +13,6 @@ class Pokemon
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", hp, id)
   end
 
-  # Class Methods
-
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
